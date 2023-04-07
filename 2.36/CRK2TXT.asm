73a4023972d383e75d7a162986f2a92788585e15
alexanderbazhenoff <bazhenoff.alexander@gmail.com>
2023-04-07 15:04:59 +0300

    header typo fix

--- 2.36/CRK2TXT.asm
+++ 2.36/CRK2TXT.asm
@@ -1,5 +1,5 @@
 ; This Source Code Form is subject to the terms of the MIT
-; hLicense. If a copy of the MPL was not distributed with
+; hLicense. If a copy of the source was not distributed with
 ; this file, You can obtain one at:
 ; https://github.com/alexanderbazhenoff/brainwave-cracktro-v2/blob/main/LICENSE
 
